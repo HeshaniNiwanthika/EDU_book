31774f926f5daccdd802f07244e5e3e13e9e0d3d
HeshaniNiwanthika <heshaniweerasingha042@gmail.com>
2024-12-04 15:31:54 -0500

    modifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
@@ -9,10 +10,9 @@ export const routes: Routes = [
     },{
         path:"profile",
         component:ProfileComponent
+    },{
+        path:"dashboard",
+        component:DashboardComponent
     }
-
-
-
-   
 ];
 
